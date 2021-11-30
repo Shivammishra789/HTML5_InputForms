63d87fc7869acbc108eb6696d3690db6289d7ea1
Shivammishra789 <shivammishra7789@gmail.com>
2021-11-30 23:10:34 +0530

    ADDED: applied validation for password input field

--- js/simple_form.js
+++ js/simple_form.js
@@ -15,8 +15,7 @@
  const textError = document.querySelector('.text-error');
  text.addEventListener('input', function() {
      let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
-     if(nameRegex.test(text.value))
-         textError.textContent = "";
+     if(nameRegex.test(text.value)) textError.textContent = "";
      else textError.textContent = "Name is Incorrect";
  });
 
@@ -27,8 +26,7 @@
   const emailError = document.querySelector('.email-error');
   email.addEventListener('input', function() {
       let emailRegex = RegExp("^[A-Za-z0-9!#$%&*+\\\\=?`{|}~^-]+(?:\\.[A-Za-z!#$%&*+\\\\=?`{|}~^-]+)*@(?:([0-9-]{1}|[a-zA-Z]{3,5})\\.)+[a-zA-Z]{2,3}$");
-      if(emailRegex.test(email.value))
-          emailError.textContent = "";
+      if(emailRegex.test(email.value)) emailError.textContent = "";
       else emailError.textContent = "Email is Incorrect";
   });
 
